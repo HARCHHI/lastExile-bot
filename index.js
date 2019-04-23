c2532580ceaca8e5bf914c831673ba6117f66bb1
HARCHHI <harc.maxwin@gmail.com>
2019-04-23 21:44:07 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// const linebot = require('linebot');
+
+// const bot = linebot({
+//   channelId: id,
+//   channelSecret: secret,
+//   channelAccessToken: token
+// });
+
+// bot.on('message', (event) => {
+//   event.reply(event.message.text).then((data) => {
+//     // success
+//     console.log(data);
+//   }).catch((error) => {
+//     console.error(error);
+//   });
+// });
+
+// bot.listen('/linewebhook', 3000);
